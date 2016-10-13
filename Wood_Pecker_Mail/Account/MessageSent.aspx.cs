ef555598b008d1844779681802f4f1639e107557
PaulFarr <PaulFarr@PAULFARRLT>
2016-10-12 20:03:20 -0700

    Got the post working!

---
+++ Wood_Pecker_Mail/Account/MessageSent.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Wood_Pecker_Mail.Account
+{
+    public partial class MessageSent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
